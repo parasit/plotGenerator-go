d1e9ffeed56943ca76ca90ba231892f13c708282
Bartosz Bubak <bartosz.bubak@gmail.com>
2018-10-30 20:03:21 +0100

    Recommit updates

--- DataTable.go
+++ DataTable.go
@@ -1,44 +1,54 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
 )
 
-//DataTableItem basic table element
-type DataTableItem struct {
-	itemValue int
+//DataItem basic item for random choice
+type DataItem struct {
 	itemName  string
+	itemValue int
 }
 
 //DataTable basic data type for random choice
 type DataTable struct {
-	Items []DataTableItem
+	dataValues []DataItem
 }
 
 // AddValue to DataTable
-func (dt *DataTable) AddValue(value string, wage int) []DataTableItem {
-	dti := DataTableItem{wage, value}
-	dt.Items = append(dt.Items, dti)
-	return dt.Items
+func (dt *DataTable) AddValue(name string, value int) {
+	item := DataItem{name, value}
+	dt.dataValues = append(dt.dataValues, item)
+}
+
+// AddItem adds item (struct) to DataTable
+func (dt *DataTable) AddItem(item DataItem) {
+	dt.dataValues = append(dt.dataValues, item)
 }
 
 // GetSize get virtual (!) size of all elements
 func (dt DataTable) GetSize() int {
 	result := 0
-	for k := range dt.Items {
-		result += dt.Items[k].itemValue
+	for k := range dt.dataValues {
+		result += dt.dataValues[k].itemValue
 	}
 	return result
 }
 
-//GetElementForValue returns element from table at requested ID
-func (dt DataTable) GetElementForValue(value int) string {
+//GetElementAtValue returns element from table at requested ID
+func (dt DataTable) GetElementAtValue(value int) string {
 	localIndex := 0
-	for k := range dt.Items {
-		if (value >= localIndex) && (value <= localIndex+dt.Items[k].itemValue) {
-			return dt.Items[k].itemName
+	for k := range dt.dataValues {
+		if (value >= localIndex) && (value <= localIndex+dt.dataValues[k].itemValue) {
+			return dt.dataValues[k].itemName
 		}
-		localIndex += dt.Items[k].itemValue
+		localIndex += dt.dataValues[k].itemValue
 	}
 	return "None"
 }
@@ -46,14 +56,55 @@ func (dt DataTable) GetElementForValue(value int) string {
 // DumpData from DataTable in human readable format
 func (dt DataTable) DumpData() {
 	index := 1
-	for k := range dt.Items {
-		fmt.Printf("%s : ", dt.Items[k].itemName)
-		if dt.Items[k].itemValue > 1 {
-			fmt.Printf("%d-%d\n", index, index+dt.Items[k].itemValue-1)
-			index += dt.Items[k].itemValue
+	for k := range dt.dataValues {
+		fmt.Printf("%s : ", dt.dataValues[k].itemName)
+		if dt.dataValues[k].itemValue > 1 {
+			fmt.Printf("%d-%d\n", index, index+dt.dataValues[k].itemValue-1)
+			index += dt.dataValues[k].itemValue
 		} else {
 			fmt.Println(index)
 			index++
 		}
 	}
+}
+
+// ReadFromFile loads DataItems from various type of files
+func (dt *DataTable) ReadFromFile(filename string) (items int, err error) {
+	rItems := 0
+	var rErr error
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("error opening file: %v\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		// fmt.Println(line)
+		item, err := ParseInputLine(line)
+		if err == nil {
+			dt.AddItem(item)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return rItems, rErr
+}
+
+// ParseInputLine check and parse input data
+func ParseInputLine(dataIn string) (DataItem, error) {
+
+	// simple line
+	// number:value
+	re := regexp.MustCompile(`^(?P<value>\d{1,}):(?P<name>\S{1,})$`)
+	match := re.FindStringSubmatch(dataIn)
+	if len(match) > 0 {
+		value, _ := strconv.Atoi(match[1])
+		return DataItem{match[2], value}, nil
+	}
+	return DataItem{}, errors.New("Cannot parse line")
 }
--- main.go
+++ main.go
@@ -2,22 +2,19 @@ package main
 
 import (
 	"fmt"
-
-	rpgforge "github.com/parasit/rpgforge-go"
 )
 
 func main() {
 	var dt DataTable
+	// dt.dataValues = make([0]DataItem)
 	fmt.Println("Plot Generator")
 	dt.AddValue("Miecz", 1)
 	dt.AddValue("Topór", 1)
 	dt.AddValue("Sztylet", 3)
 	dt.AddValue("Pałka", 1)
+	dt.ReadFromFile("weapons.rdf")
 	fmt.Println(dt.GetSize())
 	dt.DumpData()
-	fmt.Println(dt.GetElementForValue(3))
-	roll := rpgforge.ThrowDices("1d6").Sum()
-	fmt.Println(roll)
-	fmt.Println(dt.GetElementForValue(roll))
+	fmt.Println(dt.GetElementAtValue(1))
 
 }
